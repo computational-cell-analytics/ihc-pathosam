2be0533c8faf8bd18e0f75447af8cad2547fc173
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-02-18 20:38:45 +0100

    Update apply_pathosam to properly support (masked) WSIs

--- apply_pathosam.py
+++ apply_pathosam.py
@@ -12,29 +12,29 @@ def _load_image(input_path):
     if ext == ".h5":
         with h5py.File(input_path, "r") as f:
             image = f["image"][:]
-    elif ext == ".tif":
+    elif ext in (".tif", ".tiff"):
         image = load_tif_as_zarr(input_path)
     else:
         raise ValueError(f"Unsupported file extension: {ext}.")
     return image
 
 
-def apply_pathosam(input_path, output_path, check, use_mask):
-    image = _load_image(input_path)
+def apply_pathosam(image_path, output_path, model_path, use_mask, check):
+    image = _load_image(image_path)
 
     if os.path.exists(output_path):
         with h5py.File(output_path, "r") as f:
             segmentation = f["segmentation"][:]
     else:
         predictor, segmenter = get_predictor_and_segmenter(
-            model_type="vit_b_histopathology", is_tiled=True
+            model_type="vit_b_histopathology", is_tiled=True, checkpoint=model_path,
         )
         tile_shape, halo = (376, 376), (64, 64)
-        mask = get_mask(input_path) if use_mask else None
+        mask = get_mask(image_path) if use_mask else None
         segmentation = automatic_instance_segmentation(
             predictor, segmenter, image, tile_shape=tile_shape,
             halo=halo, verbose=True, ndim=2, batch_size=16,
-            mask=mask,
+            mask_path=mask,
         )
 
     if check:
@@ -52,16 +52,17 @@ def apply_pathosam(input_path, output_path, check, use_mask):
             f.create_dataset("segmentation", data=segmentation, compression="gzip")
 
 
-# TODO: support for masking, WSI inference, semantic segmentation and/or object classification
+# TODO: semantic segmentation and/or object classification
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image_path", required=True)
-    # TODO: make the model settable
     parser.add_argument("-o", "--output_path")
+    parser.add_argument("-m", "--model_path")
+    parser.add_argument("--use_mask", action="store_true")
     parser.add_argument("--check", action="store_true")
     args = parser.parse_args()
 
-    apply_pathosam(args.image_path, args.output_path, args.check)
+    apply_pathosam(args.image_path, args.output_path, args.model_path, args.use_mask, args.check)
 
 
 if __name__ == "__main__":
